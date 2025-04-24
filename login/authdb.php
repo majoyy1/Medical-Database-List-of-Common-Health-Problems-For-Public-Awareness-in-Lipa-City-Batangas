ec8ae743c5dfe47576ba8633b4e3bb738aeaf478
Mary Joy Rodriguez <23-34864@g.batstate-u.edu.ph>
2025-04-24 21:42:09 +0800

    health-list added

--- login/authdb.php
+++ login/authdb.php
@@ -24,7 +24,7 @@ class dbLoginConn {
 
       } catch(Exception $e) {
         error_log("Connection failed: " . $e->getMessage());
-        die("Connection Failed: No Database Connected");
+        //die("Connection Failed: No Database Connected");
       }
     }
 
